11395b7a4deefc19c6f96249144a9eeb098fe148
Amna-B <amnab23456@gmail.com>
2025-05-30 09:19:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const path = require("path");
 require("dotenv").config();
 
 const chatbotRoute = require("./routes/chatbot");
@@ -12,20 +11,12 @@ const PORT = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// ✅ Serve static frontend files
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-// ✅ API routes
 app.use("/api/chat", chatbotRoute);
-
-// ✅ Serve React app for all other routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
 
 
 
